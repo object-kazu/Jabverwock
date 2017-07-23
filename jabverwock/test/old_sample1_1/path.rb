d6d427c83a92b8ddfed1aba381d80162a2c39adf
kazuyuki shimizu <kazuyukishimizu@me.com>
2017-07-23 12:55:54 +0900

    table class description added and test rewrite.

--- jabverwock/test/old_sample1_1/path.rb
+++
@@ -1,9 +0,0 @@
-require '../../lib/global/globalDef'  
-require '../../lib/global/jw'  
-require "../../lib/global/jwMulti"
-require "../../lib/global/jwOneTag"
-require "../../lib/global/jwSingle"
-require "../../lib/global/jwTable"
-require "../../lib/global/jw_CSS"
-require "../../lib/global/jw_CSS_JS"
-require "../../lib/global/press"
--- jabverwock/test/tableBasic/index.rb
+++ jabverwock/test/tableBasic/index.rb
@@ -7,9 +7,12 @@ module Jabverwock
     
   def self.iTable
     t = JWTable.new.attr(:border, "1px")
-    t.addRows ["row1 cell1", "row1 cell2" ]
-    t.addRows ["row2 cell1", "row2 cell2" ]
-    t.addRows ["row3 cell1", "row3 cell2" ]
+    
+    a1 = ["row1 cell1", "row1 cell2" ]
+    a2 = ["row2 cell1", "row2 cell2" ]
+    a3 = ["row3 cell1", "row3 cell2" ]
+    
+    t.addRows a1, a2, a3
     t
   end
   
