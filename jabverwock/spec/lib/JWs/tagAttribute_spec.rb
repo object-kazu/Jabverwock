b3410ef5f432600ce201e0cf746214c34a68bf43
kazuyuki shimizu <kazuyukishimizu@me.com>
2017-10-29 09:44:55 +0900

    spec test of tagAttribute is done

---
+++ jabverwock/spec/lib/JWs/tagAttribute_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+require_relative '../../lib/paths'
+
+using StringExtension
+
+module Jabverwock
+
+  using StringExtension
+
+  RSpec.describe 'jw basic test' do
+    subject(:inp) { INPUT.new }
+    
+    it "TagAttribute, path confirm " do
+      tm = TagAttribute.new 
+      tm.add_lang("jp")
+
+      expect(tm.aString).to eq "lang=\"jp\""
+      
+    end  
+
+    it "TagAttribute, class " do
+      tm = TagAttribute.new 
+      tm.add_cls("test" )
+
+      expect(tm.aString).to eq "class=\"test\""
+      
+    end  
+    
+    it "TagAttribute, class case 2 " do
+      tm = TagAttribute.new 
+      tm.add_font_size("test" )
+
+      expect(tm.aString).to eq "font-size=\"test\""
+      
+    end  
+
+   
+    
+  end  
+    
+
+end
